a825e2c15fd105cdc5b73989c54aa592b26eecaf
ABHISHEK KUMAR RAJ <24mcmc24@uohyd.ac.in>
2025-08-10 06:08:29 +0000

    adding interface of registerUser

---
+++ SE_L2/SE_L2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+typedef struct registerUser
+{
+    char name[150];
+    int mob;
+    char email[50];
+    char address[100];
+    float CMR;
+    float OMR;
+    bool loan;
+}RU;
